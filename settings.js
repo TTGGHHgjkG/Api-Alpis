d35e6793a43f897043930b5303fde33fc829c404
TTGGHHgjkG <136689391+TTGGHHgjkG@users.noreply.github.com>
2024-02-05 18:06:45 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,7 +20,7 @@ require('dotenv').config()
 //_______________________ ┏  Main Setting  ┓ _______________________\\
 
 creator = '乂𝘼𝙡𝙞𝙥乂' // Nama 
-port = 8080 //port host
+port = process.env.PORT || 3000
 LimitApikey = 200 // Limit Apikey default
 
 //___________ ┏  Database Mongodb Setting  ┓ ___________\\
